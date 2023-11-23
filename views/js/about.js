5257851df7588e4d41cb9bdd6be1f4ef571b88b6
gpulch <42658765+gpulch@users.noreply.github.com>
2023-11-23 14:28:04 +0100

    form no plug

--- views/js/about.js
+++ views/js/about.js
@@ -1,1 +1,13 @@
 console.log('oker')
+
+const repos = [
+    'repo1',
+    'repo2',
+    'repo3'
+]
+
+// create a map function to return a list of repos with a link to the repo as list items
+const repoList = repos.map(repo => {
+    return `<li><a href="${repo}">${repo}</a></li>`
+})
+console.log(repoList)
